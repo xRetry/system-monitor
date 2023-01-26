64cd56d367e87bdbac0025767420793d43e2b5d9
xRetry <marcoschwarz.at@gmail.com>
2023-01-26 19:41:43 +0100

    Add program manager

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::{Command, Output, Stdio};
+use std::string::FromUtf8Error;
 
 #[derive(Debug)]
 struct CommandParseError {}
@@ -22,32 +23,66 @@ impl<'a> Program<'a> {
         };
     }
 
-    pub fn check(&self) {
-        self.compare_to_expected(self.cmd_status, self.expected_status);
+    pub fn check(&self) -> Result<bool, FromUtf8Error> {
+        let is_ok = self.compare_to_expected(self.cmd_status, self.expected_status)?;
+        return Ok(is_ok);
     }
 
-    pub fn start(&self) {
-        self.compare_to_expected(self.cmd_start, self.expected_start);
+    pub fn start(&self) -> Result<bool, FromUtf8Error> {
+        let is_ok = self.compare_to_expected(self.cmd_start, self.expected_start)?;
+        return Ok(is_ok);
     }
 
-    fn compare_to_expected(&self, cmd: &str, expected: Option<&str>) -> bool {
+    fn compare_to_expected(
+        &self,
+        cmd: &str,
+        expected: Option<&str>,
+    ) -> Result<bool, FromUtf8Error> {
         let output = exec_command(cmd).unwrap();
 
         match expected {
             Some(exp) => {
-                let out_str = String::from_utf8(output.stdout).unwrap();
+                let out_str = String::from_utf8(output.stdout)?;
                 if out_str == exp {
-                    return true;
+                    return Ok(true);
                 }
-                return false;
+                return Ok(false);
             }
             None => {
-                let out_str = String::from_utf8(output.stderr).unwrap();
+                let out_str = String::from_utf8(output.stderr)?;
                 if out_str.len() == 0 {
-                    return true;
+                    return Ok(true);
                 }
-                return false;
+                return Ok(false);
             }
+        }
+    }
+}
+
+struct ProgramManager<'a> {
+    programs: Vec<Program<'a>>,
+}
+
+impl<'a> ProgramManager<'a> {
+    fn new() -> ProgramManager<'a> {
+        return ProgramManager {
+            programs: Vec::<Program>::new(),
+        };
+    }
+
+    fn add_program(&mut self, prog: Program<'a>) {
+        self.programs.push(prog);
+    }
+
+    fn start_programs(&mut self) {
+        for program in self.programs.iter() {
+            program.start();
+        }
+    }
+
+    fn check_programs(&mut self) {
+        for program in self.programs.iter() {
+            program.check();
         }
     }
 }
@@ -66,6 +101,10 @@ fn exec_command(cmd_string: &str) -> Result<Output, CommandParseError> {
 }
 
 fn main() {
+    let mut manager = ProgramManager::new();
+
     let prog = Program::new("Test".to_owned());
-    prog.check();
+    manager.add_program(prog);
+    manager.start_programs();
+    manager.check_programs();
 }
